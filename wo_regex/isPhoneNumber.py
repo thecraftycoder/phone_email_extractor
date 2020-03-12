7ef7acf5f248e90b940c3aabde7696badc83723e
the crafty coder <lisathuanguyen@gmail.com>
2020-03-12 11:05:22 -0700

    example code -  checking a phonenumber wout regex

--- wo_regex/isPhoneNumber.py
+++ wo_regex/isPhoneNumber.py
@@ -7,3 +7,34 @@ Ex: 415-555-4242
 
 Returns: True or False
 '''
+
+def isPhoneNumber(text):
+    # is the string exactly 12 characters?
+    if len(text) != 12:
+        return False
+    # Checking area code: are the first 3 characters integers?
+    for i in range(0,3):
+        if not text[i].isdecimal():
+            return False
+    # Checking the rest of the pattern: is the 4th character a hyphen?
+    if text[3] != '-':
+        return False
+    # Do three more numbers follow the first hyphen (index 4, 5, 6)
+    for i in range(4,7):
+        if not text[i].isdecimal():
+            return False
+    # is the 8th character a hyphen?
+    if text[7] != '-':
+        return False
+    # Do four numbers follow the second hyphen (index 8, 9, 10, 11)
+    for i in range(8,12):
+        if not text[i].isdecimal():
+            return False
+    # If the string passes all the checks, return True
+    return True
+
+print('Is 415-555-4242 a phone number?')
+print(isPhoneNumber('415-555-4242'))
+
+print('Is Moshi moshi a phone number?')
+print(isPhoneNumber('Moshi moshi'))
